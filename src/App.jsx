6a1a5ad239ab176fe92687c2e96d16452839b73c
chjh6107 <chjh6107@gmail.com>
2021-12-22 20:38:08 +0900

    22

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Hello from './components/Hello'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const name='react';
+  const style={
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:24,
+    display:'flex'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Hello name="react"/>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+const Hello = (props) => {
+    console.log('hello render');
+    return (<div>하잉 {props.name} </div>);
+}
+export default Hello;
